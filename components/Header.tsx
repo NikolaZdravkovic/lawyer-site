747b20b02361f19232d2cfcc2e75ee046bb0162d
Nikola Zdravkovic <zdravko.dzoni@gmail.com>
2024-01-18 22:02:15 +0100

    add navbar / components

---
+++ components/Header.tsx
@@ -0,0 +1,45 @@
+/* eslint-disable @next/next/no-html-link-for-pages */
+import Navigation from './Navigation'
+import Image from 'next/image';
+import heroImg from '../public/images/banner.png'
+
+
+export default function Header() {
+
+  return (
+    <>
+      <div className='banner bg-slate-900'>
+        <div className='banner-image relative md:h-[900px]'>
+          <Image
+            fill
+            style={{objectFit:"cover"}}
+            src={`${heroImg.src}`}
+            alt="image"
+            loading="eager" priority={true} 
+          />
+          <Navigation />
+          <div className='banner-text relative flex text-white wrapper md:pr-96 pl-4 pt-24 pr-24 md:pt-7 flex-wrap'>
+            <div className='banner-text--title md:text-6xl text-3xl pb-7'>
+              Deeper understanding better solution
+            </div>
+            <div className='banner-text--desc md:text-2xl pb-7'>
+              It is a long established fact that a reader will be distracted by
+              the readable content of a looking at its layout.
+            </div>
+            <div className='banner-text--btn'>
+              <div className="py-1 text-right flex">
+                <a
+                  className="mt-2 inline-flex items-center px-8 py-3 text-lg font-semibold tracking-tighter text-black bg-[#D1B06B]"
+                  href="/"
+                >
+                  Pozovite nas
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+    </>
+  );
+}
---
+++ components/slices/CardJob.tsx
@@ -0,0 +1,148 @@
+import React from 'react'
+
+export default function CardJob() {
+    return (
+        <>
+            <div className='flex flex-wrap justify-center'>
+                <div className='card mb-6 md:mr-6 md:w-[340px] lg:w-[420px] bg-[#1F2732] pb-10'>
+                    <div className='flex flex-wrap w-[320px] md:pl-12 pb-10 pt-12 pl-10 pr-5 md:pr-0'>
+                        <div className='font-bold md:text-4xl pb-3 text-3xl'>Business Law</div>
+                        <div className='md:text-xl border-solid border-b-2 md:pb-12 pb-10'>There are many variations of passages
+                            of Lorem Ipsum available, but majority
+                            going to use a passage.</div>
+                    </div>
+                    <div className="flex pl-12 items-center	">
+                        <div className='mr-10 md:mr-14'>
+                            <svg width="59" height="51" viewBox="0 0 59 51" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <path d="M42.2705 21.0328V32.459M17.0246 21.0328L17.0246 32.459M3 27H55M8 49.5H51C54.3137 49.5 57 46.8137 57 43.5V14.5C57 11.1863 54.3137 8.5 51 8.5H8C4.68629 8.5 2 11.1863 2 14.5V43.5C2 46.8137 4.68629 49.5 8 49.5ZM40 8.5V4.5C40 2.84315 38.6569 1.5 37 1.5H21C19.3431 1.5 18 2.84315 18 4.5V8.5H40Z" stroke="#D1B06B" strokeWidth="3" />
+                            </svg>
+                        </div>
+                        <div>
+                            <a
+                                className="mt-2 inline-flex items-center px-8 py-3 text-lg font-semibold tracking-tighter text-black bg-[#D1B06B]"
+                                href="/"
+                            >
+                                Saznaj vise
+                            </a>
+                        </div>
+                    </div>
+                </div>
+                <div className='card mb-6 md:mr-6 md:w-[340px] lg:w-[420px] bg-[#1F2732] pb-10'>
+                    <div className='flex flex-wrap w-[320px] md:pl-12 pb-10 pt-12 pl-10 pr-5 md:pr-0'>
+                        <div className='font-bold md:text-4xl pb-3 text-3xl'>Business Law</div>
+                        <div className='md:text-xl border-solid border-b-2 md:pb-12 pb-10'>There are many variations of passages
+                            of Lorem Ipsum available, but majority
+                            going to use a passage.</div>
+                    </div>
+                    <div className='flex pl-12 items-center	'>
+                        <div className='mr-10 md:mr-14'>
+                            <svg width="59" height="51" viewBox="0 0 59 51" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <path d="M42.2705 21.0328V32.459M17.0246 21.0328L17.0246 32.459M3 27H55M8 49.5H51C54.3137 49.5 57 46.8137 57 43.5V14.5C57 11.1863 54.3137 8.5 51 8.5H8C4.68629 8.5 2 11.1863 2 14.5V43.5C2 46.8137 4.68629 49.5 8 49.5ZM40 8.5V4.5C40 2.84315 38.6569 1.5 37 1.5H21C19.3431 1.5 18 2.84315 18 4.5V8.5H40Z" stroke="#D1B06B" strokeWidth="3" />
+                            </svg>
+                        </div>
+                        <div>
+                            <a
+                                className="mt-2 inline-flex items-center px-8 py-3 text-lg font-semibold tracking-tighter text-black bg-[#D1B06B]"
+                                href="/"
+                            >
+                                Saznaj vise
+                            </a>
+                        </div>
+                    </div>
+                </div>
+                <div className='card mb-6 md:mr-6 md:w-[340px] lg:w-[420px] bg-[#1F2732] pb-10'>
+                    <div className='flex flex-wrap w-[320px] md:pl-12 pb-10 pt-12 pl-10 pr-5 md:pr-0'>
+                        <div className='font-bold md:text-4xl pb-3 text-3xl'>Business Law</div>
+                        <div className='md:text-xl border-solid border-b-2 md:pb-12 pb-10'>There are many variations of passages
+                            of Lorem Ipsum available, but majority
+                            going to use a passage.</div>
+                    </div>
+                    <div className='flex pl-12 items-center	'>
+                        <div className='mr-10 md:mr-14'>
+                            <svg width="59" height="51" viewBox="0 0 59 51" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <path d="M42.2705 21.0328V32.459M17.0246 21.0328L17.0246 32.459M3 27H55M8 49.5H51C54.3137 49.5 57 46.8137 57 43.5V14.5C57 11.1863 54.3137 8.5 51 8.5H8C4.68629 8.5 2 11.1863 2 14.5V43.5C2 46.8137 4.68629 49.5 8 49.5ZM40 8.5V4.5C40 2.84315 38.6569 1.5 37 1.5H21C19.3431 1.5 18 2.84315 18 4.5V8.5H40Z" stroke="#D1B06B" strokeWidth="3" />
+                            </svg>
+                        </div>
+                        <div>
+                            <a
+                                className="mt-2 inline-flex items-center px-8 py-3 text-lg font-semibold tracking-tighter text-black bg-[#D1B06B]"
+                                href="/"
+                            >
+                                Saznaj vise
+                            </a>
+                        </div>
+                    </div>
+                </div>
+                <div className='card mb-6 md:mr-6 md:w-[340px] lg:w-[420px] bg-[#1F2732] pb-10'>
+                    <div className='flex flex-wrap w-[320px] md:pl-12 pb-10 pt-12 pl-10 pr-5 md:pr-0 '>
+                        <div className='font-bold md:text-4xl pb-3 text-3xl'>Business Law</div>
+                        <div className='md:text-xl border-solid border-b-2 md:pb-12 pb-10'>There are many variations of passages
+                            of Lorem Ipsum available, but majority
+                            going to use a passage.</div>
+                    </div>
+                    <div className='flex pl-12 items-center	'>
+                        <div className='mr-10 md:mr-14'>
+                            <svg width="59" height="51" viewBox="0 0 59 51" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <path d="M42.2705 21.0328V32.459M17.0246 21.0328L17.0246 32.459M3 27H55M8 49.5H51C54.3137 49.5 57 46.8137 57 43.5V14.5C57 11.1863 54.3137 8.5 51 8.5H8C4.68629 8.5 2 11.1863 2 14.5V43.5C2 46.8137 4.68629 49.5 8 49.5ZM40 8.5V4.5C40 2.84315 38.6569 1.5 37 1.5H21C19.3431 1.5 18 2.84315 18 4.5V8.5H40Z" stroke="#D1B06B" strokeWidth="3" />
+                            </svg>
+                        </div>
+                        <div>
+                            <a
+                                className="mt-2 inline-flex items-center px-8 py-3 text-lg font-semibold tracking-tighter text-black bg-[#D1B06B]"
+                                href="/"
+                            >
+                                Saznaj vise
+                            </a>
+                        </div>
+                    </div>
+                </div>
+                <div className='card mb-6 md:mr-6 md:w-[340px] lg:w-[420px] bg-[#1F2732] pb-10'>
+                    <div className='flex flex-wrap w-[320px] md:pl-12 pb-10 pt-12 pl-10 pr-5 md:pr-0'>
+                        <div className='font-bold md:text-4xl pb-3 text-3xl'>Business Law</div>
+                        <div className='md:text-xl border-solid border-b-2 md:pb-12 pb-10'>There are many variations of passages
+                            of Lorem Ipsum available, but majority
+                            going to use a passage.</div>
+                    </div>
+                    <div className='flex pl-12 items-center	'>
+                        <div className='mr-10 md:mr-14'>
+                            <svg width="59" height="51" viewBox="0 0 59 51" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <path d="M42.2705 21.0328V32.459M17.0246 21.0328L17.0246 32.459M3 27H55M8 49.5H51C54.3137 49.5 57 46.8137 57 43.5V14.5C57 11.1863 54.3137 8.5 51 8.5H8C4.68629 8.5 2 11.1863 2 14.5V43.5C2 46.8137 4.68629 49.5 8 49.5ZM40 8.5V4.5C40 2.84315 38.6569 1.5 37 1.5H21C19.3431 1.5 18 2.84315 18 4.5V8.5H40Z" stroke="#D1B06B" strokeWidth="3" />
+                            </svg>
+                        </div>
+                        <div>
+                            <a
+                                className="mt-2 inline-flex items-center px-8 py-3 text-lg font-semibold tracking-tighter text-black bg-[#D1B06B]"
+                                href="/"
+                            >
+                                Saznaj vise
+                            </a>
+                        </div>
+                    </div>
+                </div>
+                <div className='card mb-6 md:mr-6 md:w-[340px] lg:w-[420px] bg-[#1F2732] pb-10'>
+                    <div className='flex flex-wrap w-[320px] md:pl-12 pb-10 pt-12 pl-10 pr-5 md:pr-0'>
+                        <div className='font-bold md:text-4xl pb-3 text-3xl'>Business Law</div>
+                        <div className='md:text-xl border-solid border-b-2 md:pb-12 pb-10'>There are many variations of passages
+                            of Lorem Ipsum available, but majority
+                            going to use a passage.</div>
+                    </div>
+                    <div className='flex pl-12 items-center	'>
+                        <div className='mr-10 md:mr-14'>
+                            <svg width="59" height="51" viewBox="0 0 59 51" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <path d="M42.2705 21.0328V32.459M17.0246 21.0328L17.0246 32.459M3 27H55M8 49.5H51C54.3137 49.5 57 46.8137 57 43.5V14.5C57 11.1863 54.3137 8.5 51 8.5H8C4.68629 8.5 2 11.1863 2 14.5V43.5C2 46.8137 4.68629 49.5 8 49.5ZM40 8.5V4.5C40 2.84315 38.6569 1.5 37 1.5H21C19.3431 1.5 18 2.84315 18 4.5V8.5H40Z" stroke="#D1B06B" strokeWidth="3" />
+                            </svg>
+                        </div>
+                        <div>
+                            <a
+                                className="mt-2 inline-flex items-center px-8 py-3 text-lg font-semibold tracking-tighter text-black bg-[#D1B06B]"
+                                href="/"
+                            >
+                                Saznaj vise
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
